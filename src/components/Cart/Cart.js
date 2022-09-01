ffce466f4dd732ed11b27e23f54e5868043268bc
Samuel Arguello <80545434+Sarguello12@users.noreply.github.com>
2022-09-01 09:11:22 -0500

    adde oreder form to cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,51 +1,86 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
+
 import classes from "./Cart.module.css";
 
-const Cart = props => {
-    const cartCtx = useContext(CartContext);
-    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-    const hasItems = cartCtx.items.length > 0;
+const Cart = (props) => {
+	const [isCheckout, setIsCheckout] = useState(false);
+	const [isSubmitting, setIsSubmitting] = useState();
 
-    //use .bind on function call to ensure functions recieve their required inputs
-    const cartItemRemoveHandler = id => {
-        cartCtx.removeItem(id);
-    };
+	const cartCtx = useContext(CartContext);
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
 
-    const cartItemAddHandler = item => {
-        cartCtx.addItem({...item, amount: 1});
-    };
+	//use .bind on function call to ensure functions recieve their required inputs
+	const cartItemRemoveHandler = (id) => {
+		cartCtx.removeItem(id);
+	};
 
-    const cartItems = (
-        <ul className={classes["cart-items"]}>{
-            cartCtx.items.map(item => (
-                <CartItem 
-                    key={item.id} 
-                    name={item.name} 
-                    amount={item.amount} 
-                    price={item.price} 
-                    onRemove={cartItemRemoveHandler.bind(null, item.id)} 
-                    onAdd={cartItemAddHandler.bind(null, item)}
-                />
-            ))}
-        </ul>
-    )
-    
-    return (
-        <Modal onClose={props.onClose}>
-            {cartItems}
-            <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>{totalAmount}</span>
-            </div>
-            <div className={classes.actions}>
-                <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
-        </Modal>
-    )
+	const cartItemAddHandler = (item) => {
+		cartCtx.addItem({ ...item, amount: 1 });
+	};
+
+	const orderHandler = () => {
+		setIsCheckout(true);
+	};
+
+	const submitOrderHandler = (userData) => {
+		fetch(
+			"https://food-order-app-f7db3-default-rtdb.firebaseio.com/orders.json",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					user: userData,
+					orderedItems: cartCtx.items,
+				}),
+			}
+		);
+	};
+
+	const cartItems = (
+		<ul className={classes["cart-items"]}>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
+			))}
+		</ul>
+	);
+
+	const modalActions = (
+		<div className={classes.actions}>
+			<button className={classes["button--alt"]} onClick={props.onClose}>
+				Close
+			</button>
+			{hasItems && (
+				<button className={classes.button} onClick={orderHandler}>
+					Order
+				</button>
+			)}
+		</div>
+	);
+
+	return (
+		<Modal onClose={props.onClose}>
+			{cartItems}
+			<div className={classes.total}>
+				<span>Total Amount</span>
+				<span>{totalAmount}</span>
+			</div>
+			{isCheckout && (
+				<Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+			)}
+			{!isCheckout && modalActions}
+		</Modal>
+	);
 };
 
 export default Cart;
